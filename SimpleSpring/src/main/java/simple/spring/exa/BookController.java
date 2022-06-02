5e8df39a996509329eb6e9fc1f161155c7624c99
한민기 <hk335078@naver.com>
2022-06-02 13:16:46 +0900

    [FEAT] ADD MAIN CONTROLLER AND CREATE JSP
    ADD BOOK CONTROLLER AND CREATE.JSP
    Connection method : LOCALHOST:8080/CREATE

---
+++ SimpleSpring/src/main/java/simple/spring/exa/BookController.java
@@ -0,0 +1,15 @@
+package simple.spring.exa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BookController {
+	
+	@RequestMapping(value="/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		return new ModelAndView("book/create");
+	}
+}
