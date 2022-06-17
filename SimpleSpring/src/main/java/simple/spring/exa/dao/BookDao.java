ef59af7bbd673e5e7e3f047b758650e2751d6b6b
Min Gi Han <hk335078@naver.com>
2022-06-17 10:37:06 +0900

    [INIT] UPLOAD DAO.JAVA
    UPLOAD BOOK DAO.JAVA FILE

---
+++ SimpleSpring/src/main/java/simple/spring/exa/dao/BookDao.java
@@ -0,0 +1,18 @@
+package simple.spring.exa.dao;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Repository
+public class BookDao {
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	public int insert(Map<String, Object> map) {
+		return this.sqlSessionTemplate.insert("book.insert", map);
+	}
+}
