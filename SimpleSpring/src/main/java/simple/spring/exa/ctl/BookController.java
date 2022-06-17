305d2fb47fe3cb412d962a384c679b5dcd6bcd63
Min Gi Han <hk335078@naver.com>
2022-06-17 10:36:39 +0900

    [INIT] UPLOAD CONTROLLER.JAVA
    UPLOAD BOOK CONTROLLER.JAVA FILE

--- SimpleSpring/src/main/java/simple/spring/exa/ctl/BookController.java
+++ SimpleSpring/src/main/java/simple/spring/exa/ctl/BookController.java
@@ -1,25 +1,37 @@
 package simple.spring.exa.ctl;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import simple.spring.exa.svc.BookService;
 import simple.spring.exa.svc.TestSvc;
 
 @Controller
 public class BookController {
 	
 	@Autowired
-	private TestSvc svc;
+	BookService bookService;
 	
-	@RequestMapping(value="/create", method = RequestMethod.GET)
-	public ModelAndView create() {
+	@RequestMapping(value="/create", method = RequestMethod.POST)
+	public ModelAndView createPost(@RequestParam Map<String, Object> map) {
+		ModelAndView mav = new ModelAndView();
 		
-		Integer result = svc.selectNow();
-		System.out.println("오라클 테스트 : " + result);
+		String bookId = this.bookService.create(map);
+		if(bookId == null) {
+			mav.setViewName("redirect:/create");
+		}
+		else {
+			mav.setViewName("redirect:/detail?bookId=" + bookId);
+		}
+		//Integer result = svc.selectNow();
+		//System.out.println("오라클 테스트 : " + result);
 		
-		return new ModelAndView("book/create");
+		return mav;
 	}
 }
