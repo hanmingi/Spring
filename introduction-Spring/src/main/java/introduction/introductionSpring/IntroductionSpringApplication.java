641d6e424e91cf1ff675d178af45255468cd0209
Min Gi Han <hk335078@naver.com>
2022-07-02 16:15:35 +0900

    [INIT] ADD introduction-Spring project file

---
+++ introduction-Spring/src/main/java/introduction/introductionSpring/IntroductionSpringApplication.java
@@ -0,0 +1,13 @@
+package introduction.introductionSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionSpringApplication.class, args);
+	}
+
+}
---
+++ introduction-Spring/src/test/java/introduction/introductionSpring/IntroductionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package introduction.introductionSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
