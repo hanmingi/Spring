6cf39dc3802b2a6f749c2370721b43f11b894409
Min Gi Han <hk335078@naver.com>
2022-07-03 16:32:45 +0900

    [UPD] ADD AND UPDATE CONTROLLER AND HTML

--- introduction-Spring/src/main/java/introduction/introductionSpring/controller/IntroductionController.java
+++ introduction-Spring/src/main/java/introduction/introductionSpring/controller/IntroductionController.java
@@ -3,6 +3,8 @@ package introduction.introductionSpring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IntroductionController {
@@ -11,5 +13,38 @@ public class IntroductionController {
 	public String introducion(Model model) {
 		model.addAttribute("data", "Introduction!!");
 		return "Introduction";
+	}
+	
+	@GetMapping("Introduction-MVC")
+	public String IntroductionMVC(@RequestParam("name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "Introduction-template";
+	}
+	
+	@GetMapping("Intro-string")
+	@ResponseBody
+	public String IntroString(@RequestParam("name") String name) {
+		return "Intro " + name;
+	}
+	
+	@GetMapping("Intro-api")
+	@ResponseBody
+	public Intro IntroApi(@RequestParam("name") String name) {
+		Intro intro = new Intro();
+		
+		intro.setName(name);
+		return intro;
+	}
+	
+	static class Intro{
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
 	}
 }
