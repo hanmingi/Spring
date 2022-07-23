8483c7dc0a66918d2490f1ac6680cce19aed514b
Min Gi Han <hk335078@naver.com>
2022-07-23 19:53:37 +0900

    [INIT] ADD domain project
    [INIT] ADD domain project

---
+++ domain/src/test/java/study/domain/DomainApplicationTests.java
@@ -0,0 +1,13 @@
+package study.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
