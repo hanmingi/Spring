39dd047b4f9fef0c22c395e505371780b59ae285
Min Gi Han <hk335078@naver.com>
2022-07-24 15:51:10 +0900

    [CLN] FIX MemberController

--- introduction-Spring/src/main/java/introduction/introductionSpring/controller/MemberController.java
+++ introduction-Spring/src/main/java/introduction/introductionSpring/controller/MemberController.java
@@ -1,6 +1,9 @@
 package introduction.introductionSpring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import introduction.introductionSpring.service.MemberService;
 
 @Controller
 public class MemberController {
@@ -8,6 +11,6 @@ public class MemberController {
 	
 	@Autowired
 	public MemberController(MemberService memberService) {
-		this.memerService = memberService;
+		this.memberService = memberService;
 	}
 }
