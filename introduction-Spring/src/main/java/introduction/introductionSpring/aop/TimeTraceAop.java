ef25ef382af97befbd8f1f7ab18bbd17ba2b249b
Min Gi Han <hk335078@naver.com>
2022-08-14 13:46:42 +0900

    [FEAT] TimeTraceAop test code

--- introduction-Spring/src/main/java/introduction/introductionSpring/aop/TimeTraceAop.java
+++ introduction-Spring/src/main/java/introduction/introductionSpring/aop/TimeTraceAop.java
@@ -1,8 +1,26 @@
 package introduction.introductionSpring.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class TimeTraceAop {
-
+	
+	@Around("execution(* introduction.introductionSpring..*(..))")
+	public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+		long start = System.currentTimeMillis();
+		System.out.println("START: " + joinPoint);
+		
+		try {
+			return joinPoint.proceed();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+		}
+		
+	}
 }
